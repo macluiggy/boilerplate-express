9c30321df51bbcebfaa4b425d392d79ba16f485d
macluiggy <ferrinluiggy@gmail.com>
2021-09-20 11:46:47 -0500

    jejej

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ var app = express();
 
 module.exports = app;
 
+//esta estructura es app.METHOD.(PATH, HANDLER). METHOD es un metodo http, PATH es un directorio
+//especifico, HANDLER es una funcion que que Express llama cuando el PATH encaja, el HANDLER toma
+//los parametros req y res, req es un objeto de peticion y res es un objeto de respuesta
 app.get("/", (req, res) => {
   res.send('Hello Express');
 });
