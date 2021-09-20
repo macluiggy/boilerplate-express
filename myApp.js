d50740a27a7b9465571316c37094c9987b615c1b
macluiggy <ferrinluiggy@gmail.com>
2021-09-20 15:08:16 -0500

    using sendFile to send a file

--- myApp.js
+++ myApp.js
@@ -8,9 +8,16 @@ module.exports = app;
 //especifico, HANDLER es una funcion que que Express llama cuando el PATH encaja, el HANDLER toma
 //los parametros req y res, req es un objeto de peticion y res es un objeto de respuesta
 
+/*app.get("/", (req, res) => {
+    res.send(`/views/index.html`)
+
+})*/
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
-   
+    //usando la respuesta, se usa el metodo sendFile para que de este modo se envie el archivo
+    //html que queremos que el navegador lea, para eso necesitamos una ruta de archivo absoluta
+    //(__dirname) el cual es el directorio donde se encuentra el archivo actual (myApp.js)
+    res.sendFile(`${__dirname}/views/index.html`)
+
 })
- console.log(__dirname)
+console.log(__dirname)
 console.log('Hello World')
